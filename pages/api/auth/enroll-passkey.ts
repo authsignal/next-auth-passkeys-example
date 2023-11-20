6b97da0f1e5bc244abb0deaa3b42cef2bfc404b2
Steven Clouston <stevenclouston@gmail.com>
2023-11-20 16:18:47 +1300

    Aut 1583 improve error handling (#4)
    * Improve error handling
    
    * Add scope when enrolling a passkey
    
    * Tidy env.example

--- pages/api/auth/enroll-passkey.ts
+++ pages/api/auth/enroll-passkey.ts
@@ -1,23 +1,27 @@
 import { Authsignal } from "@authsignal/node";
 import { NextApiRequest, NextApiResponse } from "next";
-import { getToken } from "next-auth/jwt"
+import { getToken } from "next-auth/jwt";
 
 const authsignal = new Authsignal({
   secret: process.env.AUTHSIGNAL_TENANT_SECRET!,
   apiBaseUrl: process.env.NEXT_PUBLIC_AUTHSIGNAL_BASE_URL,
 });
 
-export default async function enrollPasskey(req: NextApiRequest, res: NextApiResponse) {
-  const sessionToken = await getToken({ req })
+export default async function enrollPasskey(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const sessionToken = await getToken({ req });
 
   if (!sessionToken || !sessionToken.email) {
-    return res.status(401).json('Unauthenticated');
+    return res.status(401).json("Unauthenticated");
   }
 
   const { token } = await authsignal.track({
     userId: sessionToken.email,
     action: "enroll-passkey",
+    scope: "add:authenticators",
   });
 
   res.status(200).json(token);
-};
+}
